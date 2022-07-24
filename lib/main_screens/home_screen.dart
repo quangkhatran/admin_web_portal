e8524cf69254b234b300bc4655758d02d48b4b51
quangkhatran <quangkhatran@gmail.com>
2022-07-25 06:05:04 +0700

    create Admin dashboard

--- lib/main_screens/home_screen.dart
+++ lib/main_screens/home_screen.dart
@@ -51,11 +51,35 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('Welcome to Admin Web Portal'),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.amber,
+                Colors.cyan,
+              ],
+              begin: FractionalOffset(0, 0),
+              end: FractionalOffset(1, 0),
+              stops: [0, 1],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        title: const Text(
+          'Welcome to Admin Web Portal',
+          style: TextStyle(
+            fontSize: 20.0,
+            letterSpacing: 3.0,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Padding(
               padding: const EdgeInsets.all(12.0),
@@ -63,11 +87,194 @@ class _HomeScreenState extends State<HomeScreen> {
                 timeText + '\n' + dateText,
                 style: const TextStyle(
                   fontSize: 20,
-                  color: Colors.black,
+                  color: Colors.white,
                   letterSpacing: 3.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
+            ),
+
+            // users activate and block accounts button ui
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // activate
+                ElevatedButton.icon(
+                  icon: const Icon(
+                    Icons.person_add,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Activate Users'.toUpperCase() +
+                        '\n' +
+                        'Accounts'.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                      letterSpacing: 3.0,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(40.0),
+                    primary: Colors.amber,
+                  ),
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  width: 20.0,
+                ),
+                // block
+                ElevatedButton.icon(
+                  icon: const Icon(
+                    Icons.block_flipped,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Block Users'.toUpperCase() +
+                        '\n' +
+                        'Accounts'.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                      letterSpacing: 3.0,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(40.0),
+                    primary: Colors.cyan,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+
+            // sellers activate and block accounts button uk
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // activate
+                ElevatedButton.icon(
+                  icon: const Icon(
+                    Icons.person_add,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Activate Sellers'.toUpperCase() +
+                        '\n' +
+                        'Accounts'.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                      letterSpacing: 3.0,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(40.0),
+                    primary: Colors.cyan,
+                  ),
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  width: 20.0,
+                ),
+                // block
+                ElevatedButton.icon(
+                  icon: const Icon(
+                    Icons.block_flipped,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Block Sellers'.toUpperCase() +
+                        '\n' +
+                        'Accounts'.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                      letterSpacing: 3.0,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(40.0),
+                    primary: Colors.amber,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+
+            // riders activate and block accounts button uk
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // activate
+                ElevatedButton.icon(
+                  icon: const Icon(
+                    Icons.person_add,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Activate Riders'.toUpperCase() +
+                        '\n' +
+                        'Accounts'.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                      letterSpacing: 3.0,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(40.0),
+                    primary: Colors.amber,
+                  ),
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  width: 20.0,
+                ),
+                // block
+                ElevatedButton.icon(
+                  icon: const Icon(
+                    Icons.block_flipped,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Block Riders'.toUpperCase() +
+                        '\n' +
+                        'Accounts'.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                      letterSpacing: 3.0,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(40.0),
+                    primary: Colors.cyan,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+
+            // logout button
+            ElevatedButton.icon(
+              icon: const Icon(
+                Icons.logout,
+                color: Colors.white,
+              ),
+              label: Text(
+                'Logout'.toUpperCase(),
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.white,
+                  letterSpacing: 3.0,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.all(40.0),
+                primary: Colors.amber,
+              ),
+              onPressed: () {},
             ),
           ],
         ),
