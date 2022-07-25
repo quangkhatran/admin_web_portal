8d4f1bfaab565dc4d516d1be75d0da2be1331535
quangkhatran <quangkhatran@gmail.com>
2022-07-25 07:56:11 +0700

    connect Web App with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import './authentication/login_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
