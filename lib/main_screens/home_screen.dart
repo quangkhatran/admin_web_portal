3d2957d9e252a32bbea8466ff5d23c71fc49bb0c
quangkhatran <quangkhatran@gmail.com>
2022-07-25 13:48:32 +0700

    allow admin to block sellers

--- lib/main_screens/home_screen.dart
+++ lib/main_screens/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../authentication/login_screen.dart';
+import '../sellers/all_verified_sellers_screen.dart';
 import '../users/all_blocked_users_screen.dart';
 import '../users/all_verified_users_screen.dart';
 
@@ -187,7 +188,12 @@ class _HomeScreenState extends State<HomeScreen> {
                     padding: const EdgeInsets.all(40.0),
                     primary: Colors.cyan,
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: ((c) => AllVerifiedSellersScreen())));
+                  },
                 ),
                 const SizedBox(
                   width: 20.0,
